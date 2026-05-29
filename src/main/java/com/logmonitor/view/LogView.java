9df1496445d38092675a50adbe3c16177ce0d873
Danil Parsukov <danilparsukov46@gmail.com>
2026-05-29 11:35:15 +0300

    revert: откат всех изменений проекта

--- src/main/java/com/logmonitor/view/LogView.java
+++
@@ -1,9 +0,0 @@
-package com.logmonitor.view;
-
-import com.logmonitor.model.LogEntry;
-import java.util.List;
-
-public interface LogView {
-    void renderEntries(List<LogEntry> entries);
-    void showStatus(String message);
-}
